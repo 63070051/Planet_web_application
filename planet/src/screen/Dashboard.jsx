362966f687942456eea01eb4752b843eab5e8db2
Nutdanaijeng <73683531+63070051@users.noreply.github.com>
2023-04-09 12:42:08 +0700

    add

--- planet/src/screen/Dashboard.jsx
+++ planet/src/screen/Dashboard.jsx
@@ -8,10 +8,33 @@ import Todo from "../assets/todo.svg";
 
 function Dashboard(props) {
   return (
-    <div>
-      <p>Dash</p>
+    <div style={{ backgroundColor: "#FBF7F0" }} className="flex">
+      <div className="container">
+        <div className="justify-between">
+          <div className="Top">
+            <div className="TopL">
+              <p style={{fontWeight: "bold",fontFamily : "jetbrains_valiable"}} className="text-4xl">Welcome Back, "jeng" </p>
+              <p style={{fontWeight: "semibold", fontFamily : "jetbrains_valiable" }} className="text-2xl">What's Up Today</p>
+            </div>
+            <div className="TopL">
+            <img className="w-24 mb-6" src={Logo} alt="" srcSet="" />
+            
+            </div>
+          </div>
+        </div>
+        <div className="Down">
+          <div  className="flex justify-between">
+            <div></div> 
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
+
+
 export default Dashboard;
+
+
