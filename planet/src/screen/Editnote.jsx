26e70a469b8af86939954b16e1b1fa47495ef13e
Nutdanaijeng <73683531+63070051@users.noreply.github.com>
2023-04-21 14:11:38 +0700

    Update note

--- planet/src/screen/Editnote.jsx
+++ planet/src/screen/Editnote.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import NavigationBar from "../component/NavigationBar";
 import Notification from "../assets/notification.svg";
 import Profile from "../assets/profile.svg";
-import Delete from "../assets/delete.svg"
-import "../fonts/Jura/static/Jura-Bold.ttf"
+import Delete from "../assets/delete.svg";
+import "../fonts/Jura/static/Jura-Bold.ttf";
 import "../fonts/Jockey_One/JockeyOne-Regular.ttf";
 import "../fonts/Kumbh_Sans/static/KumbhSans-Regular.ttf";
 import "../fonts/JetBrains_Mono/JetBrainsMono-VariableFont_wght.ttf";
 
-function Editnote(){
-    return(
-        <div className="selcet-none" style={{ backgroundColor: "#EFEADE" }}>
+function Editnote() {
+  const [update, setUpdate] = useState(false);
+  const [deleteNote, setDeletenote] = useState(false)
+  return (
+    <div className="selcet-none" style={{ backgroundColor: "#EFEADE" }}>
       <div
         className="grid grid-cols-5 gap-4 min-h-screen pr-6"
         style={{ backgroundColor: "#EFEADE" }}
@@ -40,41 +42,59 @@ function Editnote(){
           {/* Note */}
           <div
             className="mt-6 rounded-xl pt-8 px-8 pb-6 h-[85.5vh]"
-            style={{ backgroundColor: "#FBF7F0" }}>
+            style={{ backgroundColor: "#FBF7F0" }}
+          >
             <div className="text-xl flex justify-between items-center px-6 py-2">
-                <p className="text-2xl" style={{ fontFamily: "jockey" }}>
+              <p className="text-2xl" style={{ fontFamily: "jockey" }}>
                 My Note
-            </p>
-            <button className="rounded w-20 " style={{backgroundColor: "#F08D6E", color: "#FBF7F0"}}>ADD</button>
+              </p>
+              <button onClick={()=> {setUpdate(!update)}}
+                className="rounded w-20 "
+                style={{ backgroundColor: "#F08D6E", color: "#FBF7F0" }}
+              >
+                UPDATE
+              </button>
             </div>
             {/* textarea */}
             <div className="border" style={{ borderColor: "#D9DADA" }}>
-           
-                <div className="w-full mt-2 border-b-2" >
+              <div className="w-full mt-2 border-b-2">
                 <div className="text-xl flex justify-between items-center px-6 py-2">
-                <div>
-                <p className="text-6xl" style={{ color: "#B5B7B9", fontFamily: "jura" }}>
-                01
-                </p>
-                <p style={{fontFamily: "jura"}}>Enter your topic here</p>
-                </div>
-            <img className="w-9" src={Delete} alt="" />
-            </div>
-                </div>
-                <div className=" w-full flex justify-between items-center px-6 py-2 " style={{ borderColor: "#D9DADA" }}>
-                 <div>
-                 <p style={{fontFamily: "jura", color: "#00213F"}}>Enter Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic exercitationem aliquid quis? Consequuntur aspernatur voluptatem deleniti, inventore, ullam quod mollitia consequatur nemo ab facilis, voluptate tenetur excepturi sed voluptas rem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio asperiores delectus, deleniti adipisci mollitia maiores vitae aut sapiente? Perspiciatis amet consequuntur, deserunt culpa accusantium non repudiandae facilis id commodi ex pariatur placeat soluta ullam voluptatem ducimus rem ut? Eum totam ipsa dignissimos quaerat ab, deserunt autem, natus itaque ex facere sapiente reiciendis a. Ea, placeat earum? Ullam voluptas distinctio quos!</p>
+                  <div>
+                    <p
+                      className="text-6xl"
+                      style={{ color: "#B5B7B9", fontFamily: "jura" }}
+                    >
+                      01
+                    </p>
+                    <p style={{ fontFamily: "jura" }}>Enter your topic here</p>
+                  </div>
+                  <button onClick={()=> {
+              if(confirm("Are you sure delete")){
                 
-                 <p className="pt-2" style={{color: '#B5B7B9', fontFamily: 'jura'}}>33124</p>
+              }
+              else{
+                
+              }
+            }}><img className="w-9" src={Delete} alt="" /></button>
                 </div>
+              </div>
+              <div
+                className=" w-full items-center px-6 py-2 "
+                style={{ borderColor: "#D9DADA" }}
+              >
+                <div>
+                {update ? <textarea className="w-full h-[300px] bg-[#FBF7F0]" style={{fontFamily: "jura", color: "#00213F"}}>Enter Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic exercitationem aliquid quis? Consequuntur aspernatur voluptatem deleniti, inventore, ullam quod mollitia consequatur nemo ab facilis, voluptate tenetur excepturi sed voluptas rem. </textarea> : <p className="w-full" style={{fontFamily: "jura", color: "#00213F"}}>Enter Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic exercitationem aliquid quis? Consequuntur aspernatur voluptatem deleniti, inventore, ullam quod mollitia consequatur nemo ab facilis, voluptate tenetur excepturi sed voluptas rem. </p>}
+                    <p className="pt-2" style={{color: '#B5B7B9', fontFamily: 'jura'}}>33124</p>
+
+                  
                 </div>
-               
+              </div>
             </div>
-            </div>
+          </div>
         </div>
       </div>
     </div>
-    )
+  );
 }
 
 export default Editnote;
