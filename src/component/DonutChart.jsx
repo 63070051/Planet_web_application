783f9016bcf105357ade68d981ac0621273e5e79
tathusphol <63070065@kmitl.ac.th>
2023-04-26 09:45:50 +0700

    fixed dashboard

--- src/component/DonutChart.jsx
+++ src/component/DonutChart.jsx
@@ -4,27 +4,27 @@ import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const options = {
-  plugins: {
-    legend: {
-      display: false,
+function DonutChart({ todo, inprogress, done }) {
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
     },
-  },
-};
+  };
 
-const data = {
-  labels: ["Red", "Blue", "Yellow"],
-  datasets: [
-    {
-      data: [10, 5, 10],
-      backgroundColor: ["#FFAA9B", "#CFCFAB", "#75C9A8"],
-      hoverOffset: 4,
-      borderWidth: 2,
-      cutout: "80%",
-    },
-  ],
-};
-function DonutChart() {
-  return <Doughnut data={data} options={options}/>;
+  const data = {
+    labels: ["TODO", "INPROGRESS", "DONE"],
+    datasets: [
+      {
+        data: [todo, inprogress, done],
+        backgroundColor: ["#FFAA9B", "#CFCFAB", "#75C9A8"],
+        hoverOffset: 4,
+        borderWidth: 2,
+        cutout: "80%",
+      },
+    ],
+  };
+  return <Doughnut data={data} options={options} />;
 }
 export default DonutChart;
